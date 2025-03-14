d7520d271489c1bc8d355af4019f4cedb74caac4
piguycs <piguycs@pm.me>
2025-03-14 19:59:00 +0100

    using std.atomic.Value for more readable code

--- libs/core/sync.zig
+++ libs/core/sync.zig
@@ -1,18 +1,26 @@
+const std = @import("std");
+const Atomic = std.atomic.Value;
+
 pub const SpinLock = struct {
+    const State = enum(u8) { unlocked = 0, locked = 1 };
+
     name: []const u8, // good for debugging
-    locked: bool,
+    state: Atomic(State),
 
     pub fn new(name: []const u8) SpinLock {
-        return .{ .name = name, .locked = false };
+        return .{ .name = name, .state = Atomic(State).init(.unlocked) };
     }
 
-    pub fn acquire(lock: *SpinLock) void {
-        while (@atomicRmw(bool, &lock.locked, .Xchg, true, .acquire) == true) {
-            asm volatile ("pause");
+    pub fn acquire(self: *SpinLock) void {
+        while (true) {
+            switch (self.state.swap(.locked, .acquire)) {
+                .unlocked => break,
+                .locked => asm volatile ("pause"),
+            }
         }
     }
 
-    pub fn release(lock: *SpinLock) void {
-        @atomicStore(bool, &lock.locked, false, .release);
+    pub fn release(self: *SpinLock) void {
+        self.state.store(.unlocked, .release);
     }
 };
